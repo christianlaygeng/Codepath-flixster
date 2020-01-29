597829bc49f21d0d31533af51cbdd081b4904236
Christian Lay-Geng <35509282+christianlaygeng@users.noreply.github.com>
2020-01-29 10:19:43 -0800

    Initial Commit

---
+++ flixster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  flixster
+//
+//  Created by Christian Lay-Geng on 1/29/20.
+//  Copyright © 2020 Christian Lay-Geng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
