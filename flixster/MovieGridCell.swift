13084531c9e361ee6fedbe7e78b1a787ee79fa5f
Christian Lay-Geng <35509282+christianlaygeng@users.noreply.github.com>
2020-02-05 16:09:47 -0800

    Assignment 2 (Passing data and Collection View)

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Christian Lay-Geng on 1/30/20.
+//  Copyright © 2020 Christian Lay-Geng. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+
+    @IBOutlet weak var posterView: UIImageView!
+}
